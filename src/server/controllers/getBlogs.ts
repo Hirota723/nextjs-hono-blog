416ce65a05743c8aae3090ea78ff3d359f44e884
Hirota723 <koki.programming@gmail.com>
2025-03-03 20:12:36 +0900

    feat: APIを作成

---
+++ src/server/controllers/getBlogs.ts
@@ -0,0 +1,20 @@
+import { RouteHandler } from "@hono/zod-openapi";
+import { prisma } from "@/lib/prisma";
+import { getBlogsRoute } from "../routes/blogRoutes";
+
+export const getBlogsHandler: RouteHandler<typeof getBlogsRoute> = async (
+  c
+) => {
+  const blogs = await prisma.blog.findMany({
+    include: {
+      user: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+
+  return c.json(blogs, 200);
+};
